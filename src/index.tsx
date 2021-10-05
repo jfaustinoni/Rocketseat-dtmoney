f1e4c8cc19886dd00c5f0b454c94a2c55d663e8d
Jhonatan Faustinoni <jhonatanwesley.faustinoni@gmail.com>
2021-10-05 08:14:18 -0300

    Transactions and list transactions

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+    
+  },
+  
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela de site',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 5000,
+          createdAt: new Date('2021-09-30 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Freela de site',
+          type: 'withdraw',
+          category: 'Dev',
+          amount: 8000,
+          createdAt: new Date('2021-09-30 09:00:00'),
+        }
+        
+      ]
+    });
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
